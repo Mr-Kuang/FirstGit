8a06ee07c5549dac760606a6c38d94e01cedac0c
KuangXiangGan <kuangxianggan@bz.cn>
2018-12-09 12:26:55 +0800

    第一个人修改

--- src/main/java/com/git/HelloWorld.java
+++ src/main/java/com/git/HelloWorld.java
@@ -1,9 +1,11 @@
 package com.git;
 
+import java.util.Date;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World"+new Date());
 	}
 
 }
